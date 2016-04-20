3fedd31a294f0f63db8695230d112306fe5d2fee
Mustafa <mustafa.cu13@yahoo.com>
2016-04-20 15:20:31 +0200

    fixing sign up

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   
-  #devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' , :omniauth_callbacks => "callbacks" }
 
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  #devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # resources :users
   # root 'users#index'
 
